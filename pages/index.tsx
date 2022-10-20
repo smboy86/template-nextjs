342e060eb411a006c01c4dcda7a2f6d7c8b00dc0
devpoi <smboy86@naver.com>
2022-10-20 22:57:56 +0900

    config - i18n 다국어설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const Home: NextPage = () => {
+  const { t } = useTranslation('common');
+
   return (
     <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-      <h1 className='text-3xl font-bold '>hi</h1>
+      <h1 className='text-3xl font-bold mb-2'>
+        번역을 사용해보자 (프로젝트 이름)
+      </h1>
+      <div className=''>{t('title')}</div>
     </div>
   );
 };
+
+// TODO - TYPE
+export const getStaticProps = async ({ locale }: any) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])),
+  },
+});
 
 export default Home;
