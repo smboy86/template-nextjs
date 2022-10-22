bfcbbf1bd8db11efcd560f9518f084e2229943b8
devpoi <smboy86@naver.com>
2022-10-22 10:12:01 +0900

    add - theme, fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      'testFont': ['Roboto sans-serif']
+    }
   },
   variants: {
     extend: {},
