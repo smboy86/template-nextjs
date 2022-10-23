d1f3761e02ec3cb7cf9cd187f9a82e27a1196991
devpoi <smboy86@naver.com>
2022-10-23 18:34:59 +0900

    pub - 기본 인덱스 화면 정리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import type { NextPage } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   const { t } = useTranslation('common');
 
   return (
     <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-      <h1 className='text-3xl font-bold mb-2'>
-        번역을 사용해보자 (프로젝트 이름)
-      </h1>
-      <div className=''>{t('title')}</div>
+      <h1 className='text-3xl font-bold mb-2'>페이지 리스트</h1>
+      <ul>
+        <li>
+          <Link href={'./pubs/main'}>메인페이지</Link>
+        </li>
+      </ul>
     </div>
   );
 };
